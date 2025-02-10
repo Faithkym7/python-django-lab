0aedd31b52ec8b697dbf9610d4618b300686c53b
Faith Kimani <faithkym7@gmail.com>
2025-02-10 22:49:44 +0300

    Chore: creating note views

--- Django_React_FulllStack_App/backend/api/views.py
+++ Django_React_FulllStack_App/backend/api/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, NoteSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from .models import Note
 
 # Create your views here.
+# User views
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializer_class = User
+    serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+# Note views
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+    
+#    TODO: continue from 40:06 in the video - https://www.youtube.com/watch?v=c-QsfbznSXI
